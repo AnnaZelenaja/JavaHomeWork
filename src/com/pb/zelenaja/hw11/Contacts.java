1b21cefd434daf983624491ebd283a523e40d66e
Anna Zelenaja <anna.zelenaja@privatbank.ua>
2021-12-16 22:41:32 +0200

    16/12/2021

---
+++ src/com/pb/zelenaja/hw11/Contacts.java
@@ -0,0 +1,77 @@
+package com.pb.zelenaja.hw11;
+
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+import java.util.ArrayList;
+
+public class Contacts{
+    private String name;
+    private transient LocalDate dateOfBirth;
+    ArrayList<String> phone = new ArrayList<>();
+    private String address;
+    private transient LocalDateTime dateOfModif;
+
+    public Contacts() {
+    }
+
+    public Contacts(String name, LocalDate dateOfBirth, ArrayList<String> phoneList, String address) {
+        this.name = name;
+        this.dateOfBirth = dateOfBirth;
+        this.phone = phoneList;
+        this.address = address;
+        this.dateOfModif = LocalDateTime.now();
+    }
+
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public LocalDate getDateOfBirth() {
+        return dateOfBirth;
+    }
+
+    public void setDateOfBirth(LocalDate dateOfBirth) {
+        this.dateOfBirth = dateOfBirth;
+    }
+
+    public ArrayList<String> getPhone() {
+        return phone;
+    }
+
+    public void setPhone(ArrayList<String> phone) {
+        this.phone = phone;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+
+    public LocalDateTime getDateOfModif() {
+        return dateOfModif;
+    }
+
+    public void setDateOfModif(LocalDateTime dateOfModif) {
+        this.dateOfModif = dateOfModif;
+    }
+
+
+    @Override
+    public String toString() {
+        return  "Contacts{" +
+                "ФИО='" + name + '\'' +
+                ", День рождения=" + dateOfBirth +
+                ", номер телефона='" + phone + '\'' +
+                ", адресс='" + address + '\'' +
+                ", дата/время редактирования=" + dateOfModif +
+                '}'+'\n';
+    }
+}
---
+++ src/com/pb/zelenaja/hw11/Menu.java
@@ -0,0 +1,173 @@
+package com.pb.zelenaja.hw11;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.SerializationFeature;
+import com.fasterxml.jackson.databind.module.SimpleModule;
+import jdk.internal.org.objectweb.asm.TypeReference;
+
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+import java.util.*;
+import java.util.function.Consumer;
+import java.util.List;
+
+public class Menu {
+
+    static Scanner scanner = new Scanner(System.in);
+
+    static class NameComparator implements Comparator<Contacts> {
+        public int compare(Contacts a, Contacts b) {
+            return a.getName().compareTo(b.getName());
+        }
+    }
+
+    static class BComparator implements Comparator<Contacts> {
+
+        public int compare(Contacts a, Contacts b) {
+            return a.getDateOfBirth().compareTo(b.getDateOfBirth());
+        }
+    }
+
+    public static Contacts CreateContact() {
+            System.out.println("Введите ФИО\n");
+            String name = scanner.next();
+            System.out.println("Введите год рождения\n");
+            int year = scanner.nextInt();
+            System.out.println("Введите месяц рождения\n");
+            int month = scanner.nextInt();
+            System.out.println("Введите число рождения\n");
+            int day = scanner.nextInt();
+            ArrayList<String> telList = new ArrayList<>();
+            String vvod;
+            do {
+                System.out.println("Введите телефон\n");
+                String tel = scanner.next();
+                telList.add(tel);
+                System.out.println("Хотите ввести ещё номер телефона? Y/N\n");
+                vvod = scanner.next();
+            } while ((vvod.equals("Y")) || vvod.equals("y"));
+            System.out.println("Введите адресс\n");
+            String address = scanner.next();
+            Contacts contacts = new Contacts(name, LocalDate.of(year, month, day), telList, address);
+            return contacts;
+        }
+
+    public static void main(String[] args) throws Exception {
+        ArrayList<Contacts> contact = new ArrayList<>();
+        String fileName = "D://Java/Contacts.json";
+        Path fileTelBook = Paths.get(fileName);
+        ObjectMapper mapper = new ObjectMapper();
+        // pretty printing (json с отступами)
+        mapper.enable(SerializationFeature.INDENT_OUTPUT);
+        // для работы с полями типа LocalDate и LocalDateTime
+        SimpleModule module = new SimpleModule();
+        module.addSerializer(LocalDate.class, new LocalDateSerializer());
+        module.addSerializer(LocalDateTime.class, new LocalDateTimeSerializer());
+        module.addDeserializer(LocalDate.class, new LocalDateDeserializer());
+        module.addDeserializer(LocalDateTime.class, new LocalDateTimeDeserializer());
+        mapper.registerModule(module);
+
+        System.out.println("Добавление элемента");
+        String vibor;
+        do {
+        Contacts contacts = CreateContact();
+        contact.add(contacts);
+        System.out.println("Хотите ввести ещё абонента? Y/N\n");
+            vibor = scanner.next();
+        System.out.println(contact);
+        } while ((vibor.equals("Y")) || vibor.equals("y"));
+
+        System.out.println("Удаление элемента\n");
+        System.out.println("Введите ФИО абонента");
+        String delElement = scanner.next();
+        for (int i = 0; i < contact.size(); i++) {
+            if (delElement.equals(contact.get(i).getName())) {
+                contact.remove(i);
+            }
+        }
+        System.out.println(contact);
+
+        System.out.println("Поиск абонента");
+        System.out.println("Введите ФИО абонента для поиска\n");
+        String contactForDel = scanner.next();
+        boolean flag = true;
+        for (int i = 0; i < contact.size(); i++) {
+            if (contactForDel.equals(contact.get(i).getName())) {
+                System.out.println(contact.get(i));
+                flag = false;
+            }
+        }
+        if (flag) System.out.println("Абонент не найден");
+
+        System.out.println("Вывод всех записей с сортировкой\n");
+        System.out.println("Выберите поле для сортировки\n" +
+                "1 Сортировать по ФИО\n" +
+                "2 Сортировать по дате рождения\n");
+        String vvod = scanner.next();
+        if (vvod.equals("1")) {
+            NameComparator namecomp = new NameComparator();
+            TreeSet<Contacts> people = new TreeSet<>(namecomp);
+            people.addAll(contact);
+            people.forEach(new Consumer<Contacts>() {
+                @Override
+                public void accept(Contacts p) {
+                    System.out.print(p.getName() + " - ");
+                }
+            });
+            System.out.println();
+            System.out.println(people);
+        } else if (vvod.equals("2")) {
+            BComparator dcomp = new BComparator();
+            TreeSet<Contacts> abonents = new TreeSet<>(dcomp);
+            abonents.addAll(contact);
+            abonents.forEach(new Consumer<Contacts>() {
+                @Override
+                public void accept(Contacts p) {
+                    System.out.print(p.getName() + " - ");
+                }
+            });
+            System.out.println();
+            System.out.println(abonents);
+        }
+
+        System.out.println("Редактирование элемента\n");
+        System.out.println("Введите номер строки для редактирования\n");
+        int numberStr = scanner.nextInt();
+        System.out.println("Введите новые данные\n");
+        numberStr --;
+        Contacts contacts1 = CreateContact();
+        contact.set(numberStr, contacts1);
+        System.out.println(contact);
+
+        System.out.println("Запись данных в файл\n");
+        String personsJson = mapper.writeValueAsString(contact);
+        System.out.println(personsJson);
+        try (BufferedWriter writer = Files.newBufferedWriter(fileTelBook)) {
+            writer.write(personsJson);
+        }
+        catch (Exception ex){
+            ex.printStackTrace();
+        }
+
+        System.out.println("Выгрузка из файла данных");
+        try (BufferedReader reader = Files.newBufferedReader(fileTelBook)) {
+            String strFile="",line;
+            while((line = reader.readLine()) != null) {
+                strFile=strFile+line;
+            }
+            System.out.println("Содержимое телефонной книги из файла.");
+            List<Contacts> contact1 = mapper.readValue(strFile, new TypeReference<List<Contacts>>() {});
+            System.out.println(contact1);
+        } catch (Exception ex) {
+            ex.printStackTrace();
+        }
+    }
+}
+
+
+
